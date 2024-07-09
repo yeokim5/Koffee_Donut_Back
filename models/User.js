2c0cf7aec0f71c235d6eb5993bf7015e1bf4530a
unknown <kymkhw279@gmail.com>
2024-07-09 11:29:42 +0900

    google auth implement

--- models/User.js
+++ models/User.js
@@ -1,22 +1,52 @@
-const mongoose = require('mongoose')
+const mongoose = require("mongoose");
 
 const userSchema = new mongoose.Schema({
-    username: {
-        type: String,
-        required: true
+  username: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  password: {
+    type: String,
+    required: true,
+  },
+  email: {
+    type: String,
+    required: function () {
+      return this.authMethod === "google";
     },
-    password: {
-        type: String,
-        required: true
+    unique: function () {
+      return this.email != null;
     },
-    roles: {
-        type: [String],
-        default: ["Employee"]
+  },
+  name: {
+    type: String,
+    required: function () {
+      return this.authMethod === "google";
     },
-    active: {
-        type: Boolean,
-        default: true
-    }
-})
+  },
+  roles: [
+    {
+      type: String,
+      default: "Employee",
+    },
+  ],
+  active: {
+    type: Boolean,
+    default: true,
+  },
+  authMethod: {
+    type: String,
+    enum: ["local", "google"],
+    default: "local",
+  },
+});
 
-module.exports = mongoose.model('User', userSchema)
+userSchema.pre("save", function (next) {
+  if (this.authMethod === "local" && !this.email) {
+    this.email = undefined;
+  }
+  next();
+});
+
+module.exports = mongoose.model("User", userSchema);
--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -1,15 +1,11 @@
-const express = require('express')
-const router = express.Router()
-const authController = require('../controllers/authController')
-const loginLimiter = require('../middleware/loginLimiter')
+const express = require("express");
+const router = express.Router();
+const authController = require("../controllers/authController");
+const loginLimiter = require("../middleware/loginLimiter");
 
-router.route('/')
-    .post(loginLimiter, authController.login)
+router.route("/").post(loginLimiter, authController.login);
+router.route("/refresh").get(authController.refresh);
+router.route("/logout").post(authController.logout);
+router.route("/google").post(authController.googleLogin);
 
-router.route('/refresh')
-    .get(authController.refresh)
-
-router.route('/logout')
-    .post(authController.logout)
-
-module.exports = router
+module.exports = router;
