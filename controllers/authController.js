286c8abaa4ba7214defabc9abf2e85128742a087
yeokim5 <kymkhw279@gmail.com>
2024-07-19 16:58:52 +0900

    visited note bug fix

--- controllers/authController.js
+++ controllers/authController.js
@@ -64,11 +64,7 @@ const logout = (req, res) => {
   const cookies = req.cookies;
   if (!cookies?.jwt) return res.sendStatus(204);
   res.clearCookie("jwt", { httpOnly: true, sameSite: "None", secure: true });
-  res.clearCookie("visitedNotes", {
-    httpOnly: true,
-    sameSite: "None",
-    secure: true,
-  });
+  res.clearCookie("visitedNotes");
   res.json({ message: "Cookie cleared" });
 };
 
