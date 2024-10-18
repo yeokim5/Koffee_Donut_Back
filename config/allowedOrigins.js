360ff9a627a1b092acdd51b759d081b1453c88eb
yeokim5 <kymkhw279@gmail.com>
2024-10-18 16:51:27 +0900

    .

--- config/allowedOrigins.js
+++ config/allowedOrigins.js
@@ -3,7 +3,7 @@ const allowedOrigins = [
   "https://localhost:3000",
   "https://koffee-donut.onrender.com",
   "https://koffee-donut.vercel.app",
-  "www.koffeed.com",
+  "https://www.koffeed.com",
 ];
 
 module.exports = allowedOrigins;
