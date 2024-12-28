b32fe8bceb226450652411b05f5a892e23724384
Dawn Kim <kymkhw279@gmail.com>
2024-12-28 21:48:57 +0900

    auth fix 4(persist print)

--- controllers/authController.js
+++ controllers/authController.js
@@ -66,12 +66,12 @@ const refresh = (req, res) => {
 const logout = (req, res) => {
   const cookies = req.cookies;
   if (!cookies?.jwt) return res.sendStatus(204);
-  // res.clearCookie("jwt", {
-  //   httpOnly: true,
-  //   sameSite: "None",
-  //   secure: true,
-  //   path: "/",
-  // });
+  res.clearCookie("jwt", {
+    httpOnly: true,
+    sameSite: "None",
+    secure: true,
+    path: "/",
+  });
   res.json({ message: "Cookie cleared" });
 };
 
