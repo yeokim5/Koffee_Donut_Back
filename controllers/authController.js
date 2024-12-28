57f84503694cdca4d09fcd65948107e9c0a0f7b5
Dawn Kim <kymkhw279@gmail.com>
2024-12-28 14:19:41 +0900

    refresh bug

--- controllers/authController.js
+++ controllers/authController.js
@@ -31,6 +31,7 @@ const login = async (req, res) => {
     secure: true,
     sameSite: "None",
     maxAge: 24 * 60 * 60 * 1000,
+    path: "/",
   });
 
   res.json({ accessToken });
@@ -65,7 +66,12 @@ const refresh = (req, res) => {
 const logout = (req, res) => {
   const cookies = req.cookies;
   if (!cookies?.jwt) return res.sendStatus(204);
-  res.clearCookie("jwt", { httpOnly: true, sameSite: "None", secure: true });
+  res.clearCookie("jwt", {
+    httpOnly: true,
+    sameSite: "None",
+    secure: true,
+    path: "/",
+  });
   res.json({ message: "Cookie cleared" });
 };
 
