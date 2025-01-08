597d1758aa70f0dfe87e7c984264c1446b30e153
Dawn Kim <kymkhw279@gmail.com>
2025-01-08 20:56:56 +0900

    allow origin

--- config/allowedOrigins.js
+++ config/allowedOrigins.js
@@ -2,6 +2,7 @@ const allowedOrigins = [
   "http://localhost:3000",
   "https://localhost:3000",
   "https://koffee-donut.onrender.com",
+  "https://koffee-donut-back.vercel.app",
   "https://koffee-donut.vercel.app",
   "https://www.koffeed.com",
 ];
