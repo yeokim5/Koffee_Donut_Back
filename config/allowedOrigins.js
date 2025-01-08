b9305e56efbde24485dafd816696429b9fc65a1e
Dawn Kim <kymkhw279@gmail.com>
2025-01-08 21:08:25 +0900

    Remove trailing slash from allowed origin URL in configuration

--- config/allowedOrigins.js
+++ config/allowedOrigins.js
@@ -4,7 +4,6 @@ const allowedOrigins = [
   "https://koffee-donut.onrender.com",
   "https://koffee-donut.vercel.app",
   "https://www.koffeed.com",
-  "https://www.koffeed.com/",
 ];
 
 module.exports = allowedOrigins;
