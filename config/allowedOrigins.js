1b0bdb0ccfcb9c1bd942b2db0e9f1f0ff31cfc8e
Dawn Kim <kymkhw279@gmail.com>
2025-03-27 01:04:06 +0900

    cors fix

--- config/allowedOrigins.js
+++ config/allowedOrigins.js
@@ -1,7 +1,7 @@
 const allowedOrigins = [
   "http://localhost:3000",
   "https://localhost:3000",
-  "https://koffee-donut.onrender.com",
+  // "https://koffee-donut.onrender.com",
   "https://koffee-donut.vercel.app",
   "https://www.koffeed.com",
   "https://koffee-donut-back.vercel.app",
